d6122b93d1cdd9fcfa18ff27317ee1b8e81367c7
AmeerHamza185847 <hamzaraza185847@gmail.com>
2024-09-17 10:38:56 -0700

    PUT request done

--- server.mjs
+++ server.mjs
@@ -1,4 +1,5 @@
 import { createServer } from 'node:http';
+import { json } from 'stream/consumers';
 const PORT = 3000;
 
 const myServer = createServer((req, res) => {
@@ -7,12 +8,13 @@ const myServer = createServer((req, res) => {
     const baseUrl = `http://${req.headers.host}`;
     const myUrl = new URL(url, baseUrl);
 
-    // Get method
+    // Get Request
     if (method === 'GET' && myUrl.pathname === '/api/items') {
         res.writeHead(200, { 'Content-type': 'application/json' })
         res.end(JSON.stringify({ message: 'Get request ==> Fetching all items' }))
     }
-    else if ( method === 'POST' && myUrl.pathname === '/api/items/addItems') {
+    // POST Request
+    else if (method === 'POST' && myUrl.pathname === '/api/items/addItems') {
         let body = '';
         req.on('data', chunk => {
             body += chunk.toString();
@@ -25,9 +27,27 @@ const myServer = createServer((req, res) => {
             res.end(JSON.stringify({ message: "Post request ===> ", Data: newItem }));
         });
     }
+    // PUT request
+    else if (method === "PUT" && myUrl.pathname.startsWith('/api/items/')) {
+        let body = '';
+        const itemId = myUrl.pathname.split('/').pop();
+        console.log("itemId :", itemId);
+
+        req.on('data', chunk => {
+            body += chunk.toString();
+        })
+
+        req.on('end', () => {
+            const updatedItem = JSON.parse(body);
+            res.writeHead(200, { 'Content-type': 'application/json' })
+            res.end(JSON.stringify({ message: 'PUT request ==>', Data: updatedItem }))
+        })
+
+    }
+
     else {
-        res.writeHead(404,{'content-type':'application/json'})
-        res.end({message:'404 NOT FOUND'})
+        res.writeHead(404, { 'content-type': 'application/json' })
+        res.end({ message: '404 NOT FOUND' })
     }
 
 })
